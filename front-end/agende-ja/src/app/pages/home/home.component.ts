0603862d3edccc21354da684db5369bbf656d065
Pedro Paiter <pedropaiter26@gmail.com>
2024-02-20 14:05:44 -0300

    Criado component entrar e cadastrar

--- front-end/agende-ja/src/app/pages/home/home.component.ts
+++ front-end/agende-ja/src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BtnJoinComponent } from '../../components/buttons/btn-join/btn-join.component';
+import { BtnRegComponent } from '../../components/buttons/btn-reg/btn-reg.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BtnJoinComponent],
+  imports: [BtnJoinComponent, BtnRegComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
