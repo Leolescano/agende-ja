9aa5df738ed4524d543b3130cdaf1b1c5b14ec1a
evely <evelylopes290691@gmail.com>
2024-02-20 17:59:40 -0300

    Botao de criar e agendar

---
+++ front-end/agende-ja/src/app/components/buttons/bnt-create/btn-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-create.component.html',
+  styleUrl: './btn-create.component.css'
+})
+export class BtnCreateComponent {
+
+}
---
+++ front-end/agende-ja/src/app/components/buttons/bnt-schedule/btn-schedule.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-schedule',
+  standalone: true,
+  imports: [],
+  templateUrl: './btn-schedule.component.html',
+  styleUrl: './btn-schedule.component.css'
+})
+export class BtnScheduleComponent {
+
+}
